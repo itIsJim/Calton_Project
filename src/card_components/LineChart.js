e93e8737cba7cfb82869fa1631d2534473b0dc0f
Jim <jimmao25@gmail.com>
2023-04-03 16:26:38 +0200

    calton_exercise_project_init

---
+++ src/card_components/LineChart.js
@@ -0,0 +1,13 @@
+import { Paper, Typography, Grid} from "@mui/material";
+import {makeStyles,} from "@mui/styles"
+
+export default function SparklineChart () {
+    return (
+        <Paper>
+            <Grid item>
+                <Typography variant="h6" sx={{ fontWeight: 'bold' }}>Revenue</Typography>
+                
+            </Grid>
+        </Paper>
+    )
+}
